02d28aac86ffbd3964f79c218a6af3af44080bce
kipkoech177 <kipkoech.kiptoon@gmail.com>
2023-02-16 16:54:31 -0800

    task 6

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
  * main - printing a line with puts function
+ *
  * Return : 0
  */
 int main(void)
--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 /**
  * main - a program that prints a word using printf
+ *
  * Return : 0 (success)
  */
 int main(void)
