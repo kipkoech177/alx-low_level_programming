082ebd50cc4199073c42dfa4dd9b2eec1c140f7b
kipkoech177 <kipkoech.kiptoon@gmail.com>
2023-03-12 07:28:13 -0700

    the library

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strlen - returns the length of a string
+ * @s: string
+ * Return: length
+ */
+int _strlen(char *s)
+{
+	int length = 0;
+
+	if (*s != '\0')
+	{
+		lenght++;
+		s++;
+	}
+	return (lenght);
+}
