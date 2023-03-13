d8a39972b2ac2ade38e075bdb740ada265bb0ff1
kipkoech177 <kipkoech.kiptoon@gmail.com>
2023-03-12 23:20:40 -0700

    task 1

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#inlude "main.h"
+/*
+ * main - a program that prints its name
+ * @agrc: count the number of parametes
+ * @agrv: the array of characters that are entered int
+ * Return: Always 0 (succes)
+ */
+int main(int argc __attribute__((unused)), char *argv[]);
+{
+	printf("%s", *argv);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+the argv and argc projects
